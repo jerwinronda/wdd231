2f753081f63969e6b14ae579c0e61d6a8c662e8d
jerwinronda <jronda@byupathway.edu>
2026-01-14 19:32:36 +0800

    fetch api

---
+++ scripts/prophets.js
@@ -0,0 +1,37 @@
+const url =
+  "https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json";
+const cards = document.querySelector("#cards");
+
+async function getProphetData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.table(data.prophets); //temporary testing of data response
+  displayProphets(data.prophets);
+}
+
+getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    let card = document.createElement("section");
+    let fullName = document.createElement("h2");
+    let portrait = document.createElement("img");
+
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+    portrait.setAttribute("src", prophet.imageurl);
+    portrait.setAttribute(
+      "alt",
+      `portrait of ${prophet.name} ${prophets.lastname}`
+    );
+    portrait.setAttribute("loading", "lazy");
+    portrait.setAttribute("width", "340");
+    portrait.setAttribute("height", "440");
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); //fill in the blank
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  }); // end of arrow function and forEach loop
+};
