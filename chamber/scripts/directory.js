0f16bc6846880a6e41d1563982a0eb8309e22c07
jerwinronda <jronda@byupathway.edu>
2026-01-18 00:07:14 +0800

    list view addded

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,7 +1,80 @@
-const menuBtn = document.querySelector("#menuBtn");
-const navMenu = document.querySelector(".navMenu");
+// Navigation menu
+const hamButton = document.querySelector("#menu-button");
+const navBar = document.querySelector(".navigation");
 
-menuBtn.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
-  menuBtn.classList.toggle("open");
+hamButton.addEventListener("click", () => {
+  hamButton.classList.toggle("show");
+  navBar.classList.toggle("show");
+});
+
+const membersContainer = document.querySelector("#members");
+const url = "data/members.json";
+
+async function getMembers() {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+    displayMembers(data.members); // 👈 IMPORTANT
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function displayMembers(members) {
+  const membersContainer = document.querySelector("#members");
+
+  members.forEach((member) => {
+    const card = document.createElement("section");
+    card.classList.add("member-card");
+
+    card.innerHTML = `
+      <div class="image-container">
+        <img src="${member.image}" alt="${member.name} Logo" />
+      </div>
+      <h3>${member.name}</h3>
+      <p><strong>Address:</strong> ${member.address}</p>
+      <p><strong>Phone:</strong> ${member.phone}</p>
+      <p><strong>Membership Level:</strong> ${getMembershipLevel(
+        member.membership
+      )}</p>
+      <p>${member.description}</p>
+      <a href="${member.website}" target="_blank">Visit Website</a>
+    `;
+
+    membersContainer.appendChild(card);
+  });
+}
+
+// Convert membership number to label
+function getMembershipLevel(level) {
+  switch (level) {
+    case 1:
+      return "Bronze";
+    case 2:
+      return "Silver";
+    case 3:
+      return "Gold";
+    default:
+      return "Unknown";
+  }
+}
+
+// Call the function
+getMembers();
+const gridButton = document.querySelector("#grid-view");
+const listButton = document.querySelector("#list-view");
+
+gridButton.addEventListener("click", () => {
+  membersContainer.classList.add("grid");
+  membersContainer.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+  membersContainer.classList.add("list");
+  membersContainer.classList.remove("grid");
 });
